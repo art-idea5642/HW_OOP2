89e803007f38b2f99256d08a5eb5f18d2d27e6b4
art-idea5642 <chertezhy.ru@gmail.com>
2024-05-19 12:01:34 +0400

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Bicycle bicycle1 = new Bicycle("Аист", 2);
+        Car car1 = new Car("Лада", 4);
+        Truck truck1 = new Truck("Камаз", 6);
+
+        ServiceStation serviceStation = new ServiceStation();
+        serviceStation.check(bicycle1, car1, truck1);
     }
 }
